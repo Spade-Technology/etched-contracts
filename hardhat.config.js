68ce656c13921e6e79981a8823766332282feba7
manav650 <manavgarg650@gmail.com>
2023-08-07 16:03:07 +0530

    Updated configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,4 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require("@nomicfoundation/hardhat-ethers");
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-truffle5");
-
-
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.19",
 };
