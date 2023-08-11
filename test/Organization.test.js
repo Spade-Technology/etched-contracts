dcc53451ce4fb092acbbd59170670afa321dee3e
manav650 <manavgarg650@gmail.com>
2023-08-11 23:00:55 +0530

    Updated tests

---
+++ test/Organization.test.js
@@ -0,0 +1,146 @@
+const { expect } = require("chai");
+const { ethers } = require('hardhat');
+const { BigNumber } = ethers;
+
+// const { keccak256, signMessage, arrayify } = require("ethers/lib/utils");
+
+describe("Organization and Team Tests", function () {
+    let Organization, Team, Etch, organization, admin, paymaster, user1, user2, other;
+
+    // const ADMIN_ROLE = "0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775";
+    // // console.log("Admin Role: ", ADMIN_ROLE);
+
+    // const TRANSFER_ROLE = "0x8502233096d909befbda0999bb8ea2f3a6be3c138b9fbf003752a4c8bce86f6c";
+    // // console.log("Transfer Role: ", TRANSFER_ROLE);
+
+    // const DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
+    // // console.log("Default Admin Role: ", DEFAULT_ADMIN_ROLE);
+
+    let signature;
+
+    const generateSignature = async (signer, target, blocklimit = 50) => {
+        let encodedParams = ethers.utils.defaultAbiCoder.encode(["address", "uint256"], [target, await ethers.provider.getBlockNumber() + blocklimit]);
+        let hashedParams = ethers.utils.keccak256(encodedParams);
+        const signature = await signer.signMessage(ethers.utils.arrayify(hashedParams));
+        return [encodedParams, hashedParams, signature, signer.address];
+    }
+
+    before(async function () {
+        // Deploy contracts
+        Organization = await ethers.getContractFactory("Organization");
+        Team = await ethers.getContractFactory("Team");
+        Etch = await ethers.getContractFactory("Etch");
+
+        [admin, paymaster, user1, user2, other] = await ethers.getSigners();
+
+        organization = await Organization.deploy(admin.address, paymaster.address);
+        await organization.deployed();
+    });
+
+    describe("Organization Contract", function () {
+        it("should set correct initial roles", async () => {
+            const isOwner = (await organization.owner()).toString();
+            const isPaymaster = (await organization.PayMaster()).toString();
+            expect(isOwner).to.equal(admin.address.toString());
+            expect(isPaymaster).to.equal(paymaster.address.toString());
+        });
+
+        it("should allow admin to create a team", async () => {
+            let signature = await generateSignature(admin, organization.address);
+            const teamAddress = await organization.connect(paymaster).createTeam(signature);
+            const teamCount = await organization.getTeamCount();
+            expect(await teamCount.toString()).to.equal("1");
+            // expect((await organization.getTeam("0")).toString()).to.equal(teamAddress);
+        });
+
+        it("should allow non-admin to create a team", async () => {
+            let signature = await generateSignature(user1, organization.address);
+            await expect(organization.connect(paymaster).createTeam(signature)).to.be.revertedWith("Caller is not the owner");
+        });
+
+        it("should allow admin to create multiple teams", async () => {
+            let signature = await generateSignature(admin, organization.address);
+
+            await organization.connect(paymaster).createTeam(signature);
+            await organization.connect(paymaster).createTeam(signature);
+            await organization.connect(paymaster).createTeam(signature);
+
+            const teamCount = await organization.getTeamCount();
+            expect(await teamCount.toString()).to.equal("4");
+        });
+
+        it("should allow admin to create a team from admin account", async () => {
+            let signature = await generateSignature(admin, organization.address);
+            const teamAddress = await organization.connect(admin).createTeam(signature);
+            const teamCount = await organization.getTeamCount();
+            expect(await teamCount.toString()).to.equal("5");
+        });
+
+        it("should not allow admin to create a team from non-admin account", async () => {
+            let signature = await generateSignature(admin, organization.address);
+            await expect(organization.connect(user1).createTeam(signature)).to.be.revertedWith("Invalid sender");
+        });
+
+        it("Deployed team should have correct initial roles", async () => {
+            const teamAddress = await organization.getTeam("0");
+            const team = await Team.attach(teamAddress);
+            const isOwner = (await team.owner()).toString();
+            const isPaymaster = (await team.PayMaster()).toString();
+            const isOrganizationOwned = (await team.isOrganizationOwned()).toString();
+            expect(isOwner).to.equal(organization.address.toString());
+            expect(isPaymaster).to.equal(paymaster.address.toString());
+            expect(isOrganizationOwned).to.equal("true");
+        });
+
+        it("Should be able to set new paymaster", async () => {
+            let signature = await generateSignature(paymaster, organization.address);
+            await organization.connect(paymaster).setPayMaster(signature, user1.address);
+            const isPaymaster = (await organization.PayMaster()).toString();
+            expect(isPaymaster).to.equal(user1.address.toString());
+        });
+
+        it("Should not be able to set new paymaster from non-paymaster account", async () => {
+            let signature = await generateSignature(admin, organization.address);
+            await expect(organization.connect(admin).setPayMaster(signature, user1.address)).to.be.revertedWith("Can only be called by the paymaster");
+        });
+
+        it("Organization admin should be able to set default permissions for the deployed team", async () => {
+            const perm = new BigNumber.from(2); // Permission.Write
+            const teamAddress = await organization.getTeam("0");
+            const team = await Team.attach(teamAddress);
+            let signature = await generateSignature(admin, teamAddress);
+            await team.connect(paymaster).setDefaultPermission(signature, user1.address, perm);
+            const newPerm = await team.defaultPermissions(user1.address);
+            expect(newPerm.toString()).to.equal(perm.toString());
+        });
+
+        it("Organization non-admin should not be able to set default permissions for the deployed team", async () => {
+            const perm = new BigNumber.from(2); // Permission.Write
+            const teamAddress = await organization.getTeam("0");
+            const team = await Team.attach(teamAddress);
+            let signature = await generateSignature(user1, teamAddress);
+            await expect(team.connect(paymaster).setDefaultPermission(signature, user2.address, perm)).to.be.revertedWith("Caller is not the owner");
+        });
+
+        it("Organization admin should be able to set specific permissions for the deployed team", async () => {
+            const tokenId = new BigNumber.from(1000);
+            const perm = new BigNumber.from(2); // Permission.Write
+            const teamAddress = await organization.getTeam("0");
+            const team = await Team.attach(teamAddress);
+            let signature = await generateSignature(admin, teamAddress);
+            await team.connect(paymaster).setPermission(signature, user1.address, tokenId, perm);
+            const newPerm = await team.permissions(user1.address, tokenId);
+            expect(newPerm.toString()).to.equal(perm.toString());
+        });
+
+        it("Organization non-admin should not be able to set default permissions for the deployed team", async () => {
+            const tokenId = new BigNumber.from(1000);
+            const perm = new BigNumber.from(2); // Permission.Write
+            const teamAddress = await organization.getTeam("0");
+            const team = await Team.attach(teamAddress);
+            let signature = await generateSignature(user1, teamAddress);
+            await expect(team.connect(paymaster).setPermission(signature, user2.address, tokenId, perm)).to.be.revertedWith("Caller is not the owner");
+        });
+    });
+
+});
--- test/Team.test.js
+++ test/Team.test.js
@@ -4,17 +4,17 @@ const { BigNumber } = ethers;
 
 // const { keccak256, signMessage, arrayify } = require("ethers/lib/utils");
 
-describe("Team and Organization Contracts", function () {
-    let Team, team, admin, paymaster, user1, user2, other;
+describe("Team and Etch Tests", function () {
+    let Team, Etch, team, admin, paymaster, user1, user2, other;
 
-    const ADMIN_ROLE = "0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775";
-    // console.log("Admin Role: ", ADMIN_ROLE);
+    // const ADMIN_ROLE = "0xa49807205ce4d355092ef5a8a18f56e8913cf4a201fbe287825b095693c21775";
+    // // console.log("Admin Role: ", ADMIN_ROLE);
 
-    const TRANSFER_ROLE = "0x8502233096d909befbda0999bb8ea2f3a6be3c138b9fbf003752a4c8bce86f6c";
-    // console.log("Transfer Role: ", TRANSFER_ROLE);
+    // const TRANSFER_ROLE = "0x8502233096d909befbda0999bb8ea2f3a6be3c138b9fbf003752a4c8bce86f6c";
+    // // console.log("Transfer Role: ", TRANSFER_ROLE);
 
-    const DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
-    // console.log("Default Admin Role: ", DEFAULT_ADMIN_ROLE);
+    // const DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
+    // // console.log("Default Admin Role: ", DEFAULT_ADMIN_ROLE);
 
     let signature;
 
@@ -28,64 +28,39 @@ describe("Team and Organization Contracts", function () {
     before(async function () {
         // Deploy contracts
         Team = await ethers.getContractFactory("Team");
+        Etch = await ethers.getContractFactory("Etch");
 
-        [admin, paymaster, user1, user2, other] = await ethers.getSigners();        
+        [admin, paymaster, user1, user2, other] = await ethers.getSigners();
 
-        team = await Team.deploy(paymaster.address, admin.address);
+        team = await Team.deploy(admin.address, paymaster.address, 0);
         await team.deployed();
     });
 
     describe("Team Contract", function () {
         it("should set correct initial roles", async () => {
-            const isAdmin = await team.hasRole(ADMIN_ROLE, admin.address);
-            const isDefaultAdmin = await team.hasRole(DEFAULT_ADMIN_ROLE, admin.address);
+            const isOwner = (await team.owner()).toString();
             const isPaymaster = (await team.PayMaster()).toString();
-            expect(isAdmin).to.be.true;
-            expect(isDefaultAdmin).to.be.true;
+            const isOrganizationOwned = (await team.isOrganizationOwned()).toString();
+            expect(isOwner).to.equal(admin.address.toString());
             expect(isPaymaster).to.equal(paymaster.address.toString());
+            expect(isOrganizationOwned).to.equal("false");
         });
 
-        it("should allow default admin to add new admin", async () => {
-            let signature = await generateSignature(admin, team.address);
-            await team.connect(paymaster).grantRolebySignature(signature, ADMIN_ROLE, user1.address);
-            const isAdmin = await team.hasRole(await team.ADMIN_ROLE(), user1.address);
-            expect(isAdmin).to.be.true;
-        });
-
-        it("should not allow non-admin to add new admin", async () => {
-            let signature = await generateSignature(user1, team.address);
-            await expect(team.connect(paymaster).grantRolebySignature(signature, ADMIN_ROLE, user2.address)).to.be.revertedWith("Unauthorized");
-        });
-
-        it("should allow the admin to set default permissions", async () => {
+        it("Team admin should be able to set default permissions for the deployed team", async () => {
             const perm = new BigNumber.from(2); // Permission.Write
             let signature = await generateSignature(admin, team.address);
-            await team.setDefaultPermission(signature, user1.address, perm);
-            const newPerm = await team.connect(admin).defaultPermissions(user1.address);
+            await team.connect(paymaster).setDefaultPermission(signature, user1.address, perm);
+            const newPerm = await team.defaultPermissions(user1.address);
             expect(newPerm.toString()).to.equal(perm.toString());
         });
 
-        it("should not allow non-admin to set default permissions", async () => {
+        it("Team non-admin should not be able to set default permissions for the deployed team", async () => {
             const perm = new BigNumber.from(2); // Permission.Write
             let signature = await generateSignature(user1, team.address);
-            await expect(team.connect(user1).setDefaultPermission(signature, user2.address, perm)).to.be.revertedWith("Unauthorized");
+            await expect(team.connect(paymaster).setDefaultPermission(signature, user2.address, perm)).to.be.revertedWith("Caller is not the owner");
         });
 
-        it("should not allow third person to call contract to set default permissions", async () => {
-            const perm = new BigNumber.from(2); // Permission.Write
-            let signature = await generateSignature(admin, team.address);
-            await expect(team.connect(user2).setDefaultPermission(signature, user2.address, perm)).to.be.revertedWith("Invalid sender");
-        });
-
-        it("should allow the admin to set default permissions from paymaster", async () => {
-            const perm = new BigNumber.from(2); // Permission.Write
-            let signature = await generateSignature(admin, team.address);
-            await team.connect(paymaster).setDefaultPermission(signature, user2.address, perm);
-            const newPerm = await team.defaultPermissions(user2.address);
-            expect(newPerm.toString()).to.equal(perm.toString());
-        });
-
-        it("should allow the admin to set specific permissions", async () => {
+        it("Team admin should be able to set specific permissions for the deployed team", async () => {
             const tokenId = new BigNumber.from(1000);
             const perm = new BigNumber.from(2); // Permission.Write
             let signature = await generateSignature(admin, team.address);
@@ -94,35 +69,12 @@ describe("Team and Organization Contracts", function () {
             expect(newPerm.toString()).to.equal(perm.toString());
         });
 
-        it("should not allow non-admin to set specific permissions", async () => {
+        it("Team non-admin should not be able to set default permissions for the deployed team", async () => {
             const tokenId = new BigNumber.from(1000);
             const perm = new BigNumber.from(2); // Permission.Write
-            let signature = await generateSignature(user2, team.address);
-            await expect(team.connect(paymaster).setPermission(signature, user2.address, tokenId, perm)).to.be.revertedWith("Unauthorized");
+            let signature = await generateSignature(user1, team.address);
+            await expect(team.connect(paymaster).setPermission(signature, user2.address, tokenId, perm)).to.be.revertedWith("Caller is not the owner");
         });
-
-        it("should allow secondary-admin to set specific permissions", async () => {
-            let signature = await generateSignature(admin, team.address);
-            await team.connect(paymaster).grantRolebySignature(signature, ADMIN_ROLE, user1.address);
-            const isAdmin = await team.hasRole(await team.ADMIN_ROLE(), user1.address);
-            expect(isAdmin).to.be.true;
-
-            const tokenId = new BigNumber.from(1000);
-            const perm = new BigNumber.from(2); // Permission.Write
-            signature = await generateSignature(user1, team.address);
-            await team.connect(paymaster).setPermission(signature, user2.address, tokenId, perm);
-            const newPerm = await team.permissions(user2.address, tokenId);
-            expect(newPerm.toString()).to.equal(perm.toString());
-        });
-
-        it("should get correct effective permissions from default permissions", async () => {
-            const tokenId = new BigNumber.from(1000);
-            const perm = new BigNumber.from(2); // Permission.Write
-            let signature = await generateSignature(admin, team.address);
-            await team.connect(paymaster).setDefaultPermission(signature, user2.address, perm);
-            const newPerm = await team.getEffectivePermission(user1.address, tokenId);
-            expect(newPerm.toString()).to.equal(perm.toString());
-        })
 
         it("should get correct higher effective permissions from specific permissions", async () => {
             const tokenId = new BigNumber.from(1000);
@@ -145,6 +97,175 @@ describe("Team and Organization Contracts", function () {
             const newPerm = await team.getEffectivePermission(user1.address, tokenId);
             expect(newPerm.toString()).to.equal(specificPerm.toString());
         })
+
+        it("Should be able to set new paymaster", async () => {
+            let signature = await generateSignature(paymaster, team.address);
+            await team.connect(paymaster).setPayMaster(signature, user1.address);
+            const isPaymaster = (await team.PayMaster()).toString();
+            expect(isPaymaster).to.equal(user1.address.toString());
+        });
+
+        it("Deployed etch should have correct initial roles", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const name = (await etch.name()).toString();
+            const symbol = (await etch.symbol()).toString();
+            const isOwner = (await etch.owner()).toString();
+            const isPaymaster = (await etch.PayMaster()).toString();
+            expect(isOwner).to.equal(team.address.toString());
+            expect(isPaymaster).to.equal(paymaster.address.toString());
+            expect(name).to.equal("Etch");
+            expect(symbol).to.equal("ETCH");
+        });
+
+        it("Team admin should be able mint new etch for the deployed etch", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const ipfsId = "ipfs://random";
+            let signature = await generateSignature(admin, etch.address);
+            await etch.connect(paymaster).mint(signature, ipfsId);
+
+            const etchCount = await etch.totalEtches();
+            expect(await etchCount.toString()).to.equal("1");
+
+            const etchIPSFId = await etch.etches(0);
+            expect(await etchIPSFId.toString()).to.equal(ipfsId);
+        });
+
+        it("Etch contract can read the permisisions set by the team contract", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const tokenId = new BigNumber.from(0);
+            const perm = new BigNumber.from(1); // Permission.Write
+            let signature = await generateSignature(admin, team.address);
+            await team.connect(admin).setPermission(signature, user1.address, tokenId, perm);
+
+            const readPerm = await etch.canRead(user1.address, tokenId);
+            expect(readPerm).to.equal(true);
+
+            const writePerm = await etch.canWrite(user1.address, tokenId);
+            expect(writePerm).to.equal(false);
+        });
+
+        it("Team non-admin should not be able burn new etch for the deployed etch", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            let signature = await generateSignature(user1, etch.address);
+            await expect(etch.connect(paymaster).burn(signature, "0")).to.be.revertedWith("Caller is not the owner");
+        });
+
+        it("Team admin should be able burn etch for the deployed etch", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            let signature = await generateSignature(admin, etch.address);
+            await etch.connect(paymaster).burn(signature, "0");
+
+            // totalEtches should not change
+            const etchCount = await etch.totalEtches();
+            expect(await etchCount.toString()).to.equal("1");
+
+            const etchIPSFId = await etch.etches(0);
+            expect(await etchIPSFId.toString()).to.equal("");
+        });
+
+        it("Team non-admin should not be able mint new etch for the deployed etch", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const ipfsId = "ipfs://random";
+            let signature = await generateSignature(user1, etch.address);
+            await expect(etch.connect(paymaster).mint(signature, ipfsId)).to.be.revertedWith("Caller is not the owner");
+        });
+
+
+        it("Team admin should be able transfer team's etch for the deployed etch", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const ipfsId = "ipfs://random";
+            let signature = await generateSignature(admin, etch.address);
+            await etch.connect(paymaster).mint(signature, ipfsId);
+
+            await etch.connect(paymaster).transferEtch(signature, user1.address, "1");
+
+            const owner = await etch.ownerOf("1");
+            expect(await owner.toString()).to.equal(user1.address.toString());
+
+        });
+
+        it("Team non-admin should not be able transfer team's etch for the deployed etch", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const ipfsId = "ipfs://random";
+            let signature = await generateSignature(admin, etch.address);
+            await etch.connect(paymaster).mint(signature, ipfsId);
+
+            signature = await generateSignature(user1, etch.address);
+
+            await expect(etch.connect(paymaster).transferEtch(signature, user1.address, "2")).to.be.revertedWith("Caller is not the owner");
+
+        });
+
+        it("Team admin should not be able transfer non-team's etch for the deployed etch", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const ipfsId = "ipfs://random";
+            let signature = await generateSignature(admin, etch.address);
+
+            await expect(etch.connect(paymaster).transferEtch(signature, user2.address, "1")).to.be.revertedWith("ERC721: transfer from incorrect owner");
+
+
+        });
+
+        it("Account with write permission should be able to write to etch for the deployed", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const tokenId = new BigNumber.from(3);
+            const perm = new BigNumber.from(2); // Permission.Write
+            const ipfsId = "ipfs://random";
+            let signature = await generateSignature(admin, etch.address);
+            await etch.connect(paymaster).mint(signature, ipfsId);
+
+            signature = await generateSignature(admin, team.address);
+            await team.connect(admin).setPermission(signature, user2.address, tokenId, perm);
+
+            signature = await generateSignature(user2, etch.address);
+            await etch.connect(paymaster).writeToEtch(signature, tokenId, "Hello World");
+
+
+            // const etchContent = await etch.etches(tokenId);
+            // console.log(etchContent);
+            // expect(await etchContent[1][0].toString()).to.equal(user2.address.toString());
+            // expect(await etchContent[1][1].toString()).to.equal("Hello World");
+        })
+
+
+        it("Account with read permission should be able to write to etch for the deployed", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const tokenId = new BigNumber.from(2);
+            const perm = new BigNumber.from(1); // Permission.Write
+            let signature = await generateSignature(admin, team.address);
+            await team.connect(admin).setPermission(signature, user1.address, tokenId, perm);
+
+            signature = await generateSignature(user1, etch.address);
+            await expect(etch.connect(paymaster).writeToEtch(signature, tokenId, "Hello World")).to.be.revertedWith("Caller does not have write permission");
+
+
+            // const etchContent = await etch.etches(tokenId);
+            // console.log(etchContent);
+            // expect(await etchContent[1][0].toString()).to.equal(user2.address.toString());
+            // expect(await etchContent[1][1].toString()).to.equal("Hello World");
+        })
+
+        it("Owner of a particular etch should be able to write to etch for the deployed etch", async () => {
+            const etchAddress = await team.etch();
+            const etch = await Etch.attach(etchAddress);
+            const tokenId = new BigNumber.from(1);
+
+            let signature = await generateSignature(user1, etch.address);
+            await etch.connect(paymaster).writeToEtch(signature, tokenId, "Hello World");
+        });
+
+
     });
 
 });
