7aa6a5395f6aa910cae9d2f7eb601f99a4bab84a
manav650 <manavgarg650@gmail.com>
2023-08-11 17:51:16 +0530

    Updated contracts

--- contracts/Etch.sol
+++ contracts/Etch.sol
@@ -1,19 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./SignatureVerifier.sol";
+import "./Team.sol";
 
-contract Etch is ERC721, Ownable, SignatureVerifier {
+contract Etch is Ownable, ERC721, SignatureVerifier {
     uint256 public totalEtches;
-    address public teamAddress;
-    
-    enum Permission {
-        NoAccess,
-        Read,
-        Write
-    }
 
     struct Message {
         address from;
@@ -27,25 +21,14 @@ contract Etch is ERC721, Ownable, SignatureVerifier {
 
     mapping(uint256 => EtchData) public etches;
     mapping(address => uint256[]) public etchesByOwner;
-    mapping(address => mapping(uint256 => Permission)) public permissions;
-    mapping(address => Permission) public defaultPermissions;
 
-    constructor(address _owner, address _teamAddress, address _paymaster) ERC721("Etch", "ETCH") SignatureVerifier(_paymaster) {
+
+    constructor(address _owner, address _paymaster) ERC721("Etch", "ETCH") SignatureVerifier(_paymaster) {
         transferOwnership(_owner);
-        teamAddress = _teamAddress;
-    }
-
-    function _checkOwnerOrTeam(address _account) internal view {
-        require(owner() == _account || teamAddress == _account, "Caller is not the owner or team");
     }
 
     function _checkOwner(address _account) internal view {
-        require(owner() == _account, "Caller is not the owner");
-    }
-
-    function setTeamAddress(Signature memory signature, address _teamAddress) verifySignature(signature) external {
-        _checkOwner(_teamAddress);
-        teamAddress = _teamAddress;
+        require(Team(owner).owner() == _account, "Caller is not the owner");
     }
 
     function mint(Signature memory signature, string memory IPFSCID) external verifySignature(signature) returns (uint256) {
@@ -58,7 +41,7 @@ contract Etch is ERC721, Ownable, SignatureVerifier {
     }
 
     function addMessage(Signature memory signature, uint256 tokenId, string memory messageIPFSCID) external verifySignature(signature) {
-        require(getEffectivePermission(signature.signer, tokenId) == Permission.Write, "Caller does not have write permission");
+        require(Team(owner).getEffectivePermission(signature.signer, tokenId) == Team.Permission.Write, "Caller does not have write permission");
         require(ownerOf(tokenId) == owner(), "Token not owned by contract owner");
         Message memory newMessage = Message(signature.signer, messageIPFSCID);
         etches[tokenId].messages.push(newMessage);
@@ -70,39 +53,8 @@ contract Etch is ERC721, Ownable, SignatureVerifier {
         delete etches[tokenId];
     }
 
-    function getEtchesLength(address _owner) external view returns (uint256) {
-        return etchesByOwner[_owner].length;
-    }
-
-    function setDefaultPermission(Signature memory signature, address account, Permission perm) external verifySignature(signature) {
+    function transferOwnership(Signature memory signature, address newOwner) public verifySignature(signature) virtual override {
         _checkOwner(signature.signer);
-        defaultPermissions[account] = perm;
-    }
-
-    function setPermission(Signature memory signature, address account, uint256 tokenId, Permission perm) external verifySignature(signature) {
-        _checkOwnerOrTeam(signature.signer);
-        permissions[account][tokenId] = perm;
-    }
-
-    function getEffectivePermission(address account, uint256 tokenId) public view returns (Permission) {
-        Permission specificPerm = permissions[account][tokenId];
-        if (specificPerm != Permission.NoAccess) {
-            return specificPerm;
-        }
-        return defaultPermissions[account];
-    }
-
-    function canRead(address account, uint256 tokenId) external view returns (bool) {
-        return getEffectivePermission(account, tokenId) >= Permission.Read;
-    }
-
-    function canWrite(address account, uint256 tokenId) external view returns (bool) {
-        return getEffectivePermission(account, tokenId) == Permission.Write;
-    }
-
-    function transferOwnership(Signature memory signature, address newOwner) public verifySignature(signature) virtual {
-        _checkOwner(signature.signer);
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _transferOwnership(newOwner);
+        super.transferOwnership(newOwner);
     }
 }
--- contracts/Team.sol
+++ contracts/Team.sol
@@ -2,18 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "./SignatureVerifier.sol";
-import "./Organization.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
+import "./Etch.sol";
 
 /**
     @title Team Contract
     @notice This contract manages permissions and roles within a team of an organization.
  */
-contract Team is SignatureVerifier {
-
-    Organization public organization;
-
+contract Team is Ownable, SignatureVerifier {
     // Enum to describe the permission levels
     enum Permission {
         NoAccess,
@@ -21,58 +18,38 @@ contract Team is SignatureVerifier {
         Write
     }
 
+    Etch public etchContract;
+
     // Mapping from address to token ID to permission level
     mapping(address => mapping(uint256 => Permission)) public permissions;
 
     // Mapping for default permissions for users
     mapping(address => Permission) public defaultPermissions;
 
-    /**
-        @notice Initializes the Organization contract with an admin address.
-        @param _admin The address of the admin.
-     */
-     
-    constructor(
-        address _paymaster,
-        address _admin
-    ) SignatureVerifier(_paymaster) {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(ADMIN_ROLE, _admin);
+    constructor(address _owner, address _paymaster) SignatureVerifier(_paymaster) {
+        transferOwnership(_owner);
+        etchContract = new Etch(address(this), _paymaster);
+    }
+
+    function _checkOwner(address _account) internal view {
+        // require(Team(owner).owner() == _account, "Caller is not the owner");
+        require(owner() == _account, "Caller is not the owner");
     }
 
     // Function to set default permissions for a user
-    function setDefaultPermission(
-        address account,
-        Permission perm
-    ) external  {
-        hasRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    function setDefaultPermission(Signature memory signature, address account, Permission perm) external verifySignature(signature) {
+        _checkOwner(signature.signer);
         defaultPermissions[account] = perm;
     }
 
-    // Function to set default permissions for a user
-    function setDefaultPermission(
-        Signature memory signature,
-        address account,
-        Permission perm
-    ) external verifySignature(DEFAULT_ADMIN_ROLE, signature) {
-        defaultPermissions[account] = perm;
-    }
-
-    // Function to set permissions
-    function setPermission(
-        Signature memory signature,
-        address account,
-        uint256 tokenId,
-        Permission perm
-    ) external verifySignature(ADMIN_ROLE, signature) {
+    // Function to set permissions for a specific token ID
+    function setPermission(Signature memory signature, address account, uint256 tokenId, Permission perm) external verifySignature(signature) {
+        _checkOwner(signature.signer);
         permissions[account][tokenId] = perm;
     }
 
     // Function to get effective permission
-    function getEffectivePermission(
-        address account,
-        uint256 tokenId
-    ) public view returns (Permission) {
+    function getEffectivePermission(address account, uint256 tokenId) public view returns (Permission) {
         Permission specificPerm = permissions[account][tokenId];
         if (specificPerm != Permission.NoAccess) {
             return specificPerm;
@@ -81,30 +58,18 @@ contract Team is SignatureVerifier {
     }
 
     // Function to check read permission
-    function canRead(
-        address account,
-        uint256 tokenId
-    ) external view returns (bool) {
+    function canRead(address account, uint256 tokenId) external view returns (bool) {
         return getEffectivePermission(account, tokenId) >= Permission.Read;
     }
 
     // Function to check write permission
-    function canWrite(
-        address account,
-        uint256 tokenId
-    ) external view returns (bool) {
+    function canWrite(address account, uint256 tokenId) external view returns (bool) {
         return getEffectivePermission(account, tokenId) == Permission.Write;
     }
 
-    // function to transfer erc721 token
-    function transferEtch(
-        Signature memory signature,
-        address _to,
-        address _etchAddress,
-        uint256 _etchId        
-    ) external verifySignature(ADMIN_ROLE, signature) {
-        require(Organization(organization).adminAccess(_etchId, address(this)), "Team Unauthorized");
+    // function to transfer an ERC721 token
+    function transferEtch(Signature memory signature, address _to, address _etchAddress, uint256 _etchId) external {
+        _checkOwner(signature.signer);
         IERC721(_etchAddress).safeTransferFrom(address(this), _to, _etchId);
     }
-
 }
