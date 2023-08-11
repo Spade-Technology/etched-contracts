438374eca93a9f4f4f57303fe47b27a2a109f644
manav650 <manavgarg650@gmail.com>
2023-08-11 18:52:20 +0530

    Updated contracts

--- contracts/Etch.sol
+++ contracts/Etch.sol
@@ -28,7 +28,7 @@ contract Etch is Ownable, ERC721, SignatureVerifier {
     }
 
     function _checkOwner(address _account) internal view {
-        require(Team(owner).owner() == _account, "Caller is not the owner");
+        require(Team(owner()).owner() == _account, "Caller is not the owner");
     }
 
     function mint(Signature memory signature, string memory IPFSCID) external verifySignature(signature) returns (uint256) {
@@ -41,7 +41,7 @@ contract Etch is Ownable, ERC721, SignatureVerifier {
     }
 
     function addMessage(Signature memory signature, uint256 tokenId, string memory messageIPFSCID) external verifySignature(signature) {
-        require(Team(owner).getEffectivePermission(signature.signer, tokenId) == Team.Permission.Write, "Caller does not have write permission");
+        require(Team(owner()).getEffectivePermission(signature.signer, tokenId) == Team.Permission.Write, "Caller does not have write permission");
         require(ownerOf(tokenId) == owner(), "Token not owned by contract owner");
         Message memory newMessage = Message(signature.signer, messageIPFSCID);
         etches[tokenId].messages.push(newMessage);
@@ -53,8 +53,24 @@ contract Etch is Ownable, ERC721, SignatureVerifier {
         delete etches[tokenId];
     }
 
-    function transferOwnership(Signature memory signature, address newOwner) public verifySignature(signature) virtual override {
+    function transferOwnership(Signature memory signature, address newOwner) public verifySignature(signature) virtual {
         _checkOwner(signature.signer);
         super.transferOwnership(newOwner);
+    }
+
+    // Function to check read permission
+    function canRead(address account, uint256 tokenId) external view returns (bool) {
+        return Team(owner()).getEffectivePermission(account, tokenId) >= Team.Permission.Read;
+    }
+
+    // Function to check write permission
+    function canWrite(address account, uint256 tokenId) external view returns (bool) {
+        return Team(owner()).getEffectivePermission(account, tokenId) == Team.Permission.Write;
+    }
+
+     // function to transfer an Etch by owner
+    function transferEtch(Signature memory signature, address to, uint256 tokenId) external verifySignature(signature) {
+        _checkOwner(signature.signer);
+        _transfer(owner(), to, tokenId);
     }
 }
--- contracts/Team.sol
+++ contracts/Team.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./SignatureVerifier.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./SignatureVerifier.sol";
 import "./Etch.sol";
+import "./Organization.sol";
 
 /**
     @title Team Contract
     @notice This contract manages permissions and roles within a team of an organization.
  */
 contract Team is Ownable, SignatureVerifier {
+
     // Enum to describe the permission levels
     enum Permission {
         NoAccess,
@@ -18,7 +20,9 @@ contract Team is Ownable, SignatureVerifier {
         Write
     }
 
-    Etch public etchContract;
+    Etch public etch;
+
+    bool private isOrganizationOwned;
 
     // Mapping from address to token ID to permission level
     mapping(address => mapping(uint256 => Permission)) public permissions;
@@ -26,14 +30,27 @@ contract Team is Ownable, SignatureVerifier {
     // Mapping for default permissions for users
     mapping(address => Permission) public defaultPermissions;
 
-    constructor(address _owner, address _paymaster) SignatureVerifier(_paymaster) {
+    constructor(Signature memory signature, address _owner, address _paymaster, bool _isOrganizationOwned) SignatureVerifier(_paymaster) {
+        if (!_isOrganizationOwned) {
+            _checkSignature(signature);
+        }
+        isOrganizationOwned = _isOrganizationOwned;
+        etch = new Etch(address(this), _paymaster);
+        defaultPermissions[_owner] = Permission.Write;
         transferOwnership(_owner);
-        etchContract = new Etch(address(this), _paymaster);
     }
 
+    function delegateOwnership() internal view returns (address) {
+        return isOrganizationOwned ? Organization(super.owner()).owner() : super.owner();
+    }
+
+    function owner() public view override returns (address) {
+        return delegateOwnership();
+    }
+
+
     function _checkOwner(address _account) internal view {
-        // require(Team(owner).owner() == _account, "Caller is not the owner");
-        require(owner() == _account, "Caller is not the owner");
+        require(_account == delegateOwnership(), "Caller is not the owner");
     }
 
     // Function to set default permissions for a user
@@ -49,27 +66,11 @@ contract Team is Ownable, SignatureVerifier {
     }
 
     // Function to get effective permission
-    function getEffectivePermission(address account, uint256 tokenId) public view returns (Permission) {
+    function getEffectivePermission(address account, uint256 tokenId) external view returns (Permission) {
         Permission specificPerm = permissions[account][tokenId];
         if (specificPerm != Permission.NoAccess) {
             return specificPerm;
         }
         return defaultPermissions[account];
-    }
-
-    // Function to check read permission
-    function canRead(address account, uint256 tokenId) external view returns (bool) {
-        return getEffectivePermission(account, tokenId) >= Permission.Read;
-    }
-
-    // Function to check write permission
-    function canWrite(address account, uint256 tokenId) external view returns (bool) {
-        return getEffectivePermission(account, tokenId) == Permission.Write;
-    }
-
-    // function to transfer an ERC721 token
-    function transferEtch(Signature memory signature, address _to, address _etchAddress, uint256 _etchId) external {
-        _checkOwner(signature.signer);
-        IERC721(_etchAddress).safeTransferFrom(address(this), _to, _etchId);
     }
 }
