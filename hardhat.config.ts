7845247a3bb6bf7d9b5c8a7846d8e367c0bbcd19
Brieyla <business@lancedb.dev>
2023-08-29 20:44:54 +0200

    cleanup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -45,19 +45,6 @@ interface ExtendedHardhatUserConfig extends HardhatUserConfig {
 const fs = require("fs");
 const contracts = fs.readdirSync("./contracts").filter((file: string) => file.endsWith(".sol"));
 
-console.log({
-  ETHEREUM_PRIVATE_KEYS: process.env.ETHEREUM_PRIVATE_KEYS,
-  ...(!!process.env.ETHEREUM_PRIVATE_KEYS
-    ? {
-        hardhat: {
-          accounts: [...((process.env.ETHEREUM_PRIVATE_KEYS?.split(",") as string[]) || "")].map((el) => ({
-            privateKey: el,
-            balance: (100n * 10n ** 18n).toString(),
-          })),
-        },
-      }
-    : {}),
-});
 // Define the Hardhat configuration
 const config: ExtendedHardhatUserConfig = {
   solidity: {
