cc9a4555ac35c8012255be9b725887b75d0c34f2
Brieyla <business@lancedb.dev>
2023-09-08 13:10:03 +0200

    WIP Patch Wallet

--- apps/web/src/components/user-auth-form.tsx
+++ apps/web/src/components/user-auth-form.tsx
@@ -8,10 +8,21 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ConnectWalletModalButtonWrapper } from "./connect-wallet";
+import { SignIn, useAuth } from "@clerk/nextjs";
+import { useSignIn } from "@/utils/hooks/useSignIn";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+  const { signInUsingClerk } = useSignIn();
+
+  React.useEffect(() => {
+    if (sessionId && userId && isLoaded) {
+      getToken();
+    }
+  }, [userId, sessionId]);
+
   return (
     <div className={cn("grid gap-6", className)} {...props}>
       <form>
@@ -24,15 +35,14 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
           <span className="w-full border-t" />
         </div>
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
+          <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
         </div>
       </div>
-      <Button variant="outline" type="button">
+      <SignIn />
+      {/* <Button variant="outline" type="button">
         <Icons.gitHub className="mr-2 h-4 w-4" />
         Github
-      </Button>
+      </Button> */}
     </div>
   );
 }
