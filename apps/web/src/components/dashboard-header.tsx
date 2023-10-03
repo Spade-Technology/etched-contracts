19e1e5ee39200fa980a6b502731d5c6aeb9de1b4
Brieyla <business@lancedb.dev>
2023-10-03 14:10:30 +0400

    added team selector, added command line and shortcuts

--- apps/web/src/components/dashboard-header.tsx
+++ apps/web/src/components/dashboard-header.tsx
@@ -1,15 +1,25 @@
+import { CommandMenu } from "./command";
 import { CreateEtchButton } from "./create-etch-button";
 import { EtchedENS } from "./ens-button";
+import { TeamSelector } from "./team-selector";
 
 export const DashboardHeader = () => {
   return (
     <div className="flex h-16 items-center px-6 shadow-etched-1">
       <CreateEtchButton />
 
-      <div className="flex-grow" />
+      <div className="w-full flex-grow" />
+
+      <CommandMenu />
+
+      <div className="w-full flex-grow" />
 
       {/* Etched ENS */}
       <EtchedENS />
+
+      <div className="mx-6 h-full border-[1px]" />
+
+      <TeamSelector />
     </div>
   );
 };
