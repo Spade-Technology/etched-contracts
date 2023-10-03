f6d089a02cd67d815c6889fa821c4bd4892a6f34
Brieyla <business@lancedb.dev>
2023-10-03 14:41:38 +0400

    automatic update for etch creation, and added the etch created modal

--- apps/web/src/components/create-etch-button.tsx
+++ apps/web/src/components/create-etch-button.tsx
@@ -11,22 +11,18 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } fr
 import { Input } from "@/components/ui/input";
 import { api } from "@/utils/api";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Switch } from "./ui/switch";
 
-import { currentNetwork } from "@/contracts";
-import * as LitJsSdk from "@lit-protocol/lit-node-client";
-
-import { useUploadThing } from "@/utils/uploadthing";
-import { useAccount } from "wagmi";
-import { toast } from "./ui/use-toast";
-import auth from "@/pages/api/auth/[...nextauth]";
-import { useSignIn } from "@/utils/hooks/useSignIn";
 import { useQuery } from "@/gqty";
+import { useSignIn } from "@/utils/hooks/useSignIn";
+import { useUploadThing } from "@/utils/uploadthing";
+import { toast } from "./ui/use-toast";
+import { RefreshEtchesEvent } from "@/utils/hooks/useGetEtchesFromUser";
 
 const formSchema = z.object({
   etchTitle: z.string(),
@@ -39,6 +35,8 @@ type FormData = z.infer<typeof formSchema>;
 
 const previewFileTypes = ["pdf", "docx", "doc", "txt", "png", "jpg", "docx", "jpeg", "gif", "svg", "mp4", "mp3", "wav", "mpeg"];
 
+export const CreateEtchEvent = new Event("create-etch");
+
 export const CreateEtchButton = () => {
   const [fileBlobUrl, setFileBlobUrl] = React.useState<string>("");
   const { mutateAsync: mintAsync, isLoading: mintLoading } = api.etch.mintEtch.useMutation();
@@ -47,6 +45,7 @@ export const CreateEtchButton = () => {
   const { regenerateAuthSig } = useSignIn();
   const [state, setStatus] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const [etchCreated, setEtchCreated] = useState("");
   const { startUpload, isUploading } = useUploadThing("EtchUpload", {
     onUploadProgress: (progress) => setStatus(`Uploading file... (${progress}%)`),
   });
@@ -114,6 +113,8 @@ export const CreateEtchButton = () => {
         variant: "success",
       });
 
+      dispatchEvent(RefreshEtchesEvent);
+      setEtchCreated(data.etchTitle);
       setStatus("");
     } catch (e) {
       console.log(e);
@@ -149,101 +150,117 @@ export const CreateEtchButton = () => {
         </Button>
       </AlertDialogTrigger>
       <AlertDialogContent className={"max-h-screen overflow-y-scroll lg:max-w-screen-lg"}>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Create New Etch</AlertDialogTitle>
-        </AlertDialogHeader>
-
         <div className="w-full">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <div className="mb-2 flex flex-col gap-2 md:flex-row">
-                <div className="flex w-full flex-col gap-5">
-                  <FormField
-                    control={form.control}
-                    name="etchTitle"
-                    render={({ field }: { field: FieldValues }) => (
-                      <FormItem>
-                        <FormLabel>Public Etch Title</FormLabel>
-                        <FormControl>
-                          <Input disabled={isLoading} id="etchTitle" placeholder="Enter etch title" {...field} />
-                        </FormControl>
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="etchDescription"
-                    render={({ field }: { field: FieldValues }) => (
-                      <FormItem>
-                        <FormLabel>Public Etch Description (currently not in use)</FormLabel>
-                        <FormControl>
-                          <Input disabled={isLoading} id="etchDescription" placeholder="Enter etch description" {...field} />
-                        </FormControl>
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="etchFile"
-                    render={({ field }: { field: FieldValues }) => (
-                      <FormItem>
-                        <FormLabel>Upload Etch</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="file"
-                            id="etchFile"
-                            multiple={false}
-                            disabled={isLoading}
-                            onChange={(event) => {
-                              if (event.target.files?.[0]) {
-                                field.onChange(event.target.files?.[0]);
-
-                                if (previewFileTypes.includes(event.target.files?.[0].type.split("/").pop()!)) {
-                                  setFileBlobUrl(URL.createObjectURL(event.target.files?.[0]));
-                                } else {
-                                  setFileBlobUrl("UNSUPPORTED");
-                                }
-                              }
-                            }}
-                          />
-                        </FormControl>
-                        <FormDescription>Etch File: Can be set to private through encryption</FormDescription>
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="etchVisibility"
-                    render={({ field }: { field: FieldValues }) => (
-                      <FormItem className="flex flex-row items-center justify-between rounded-lg">
-                        <div className="space-y-0.5">
-                          <FormLabel className="text-base">Etch Visibility</FormLabel>
-                          <FormDescription>Choose whether your etch is public or private.</FormDescription>
-                        </div>
-                        <FormControl>
-                          <Switch disabled={isLoading} checked={field.value} onCheckedChange={field.onChange} />
-                        </FormControl>
-                      </FormItem>
-                    )}
-                  />
+          {etchCreated ? (
+            <>
+              <div className="flex flex-col items-center gap-8">
+                <h1 className="text-center text-3xl text-primary">Congratulations on your Etch! 🎉</h1>
+                <div className="text-center text-slate-500">
+                  Your Etch <span className="text-primary">{etchCreated}</span> has been created. You can view it on the dashboard
                 </div>
-                {fileBlobUrl && fileBlobUrl !== "UNSUPPORTED" && (
-                  <div>
-                    <Label>Preview</Label>
-                    <iframe src={fileBlobUrl} title="Preview" ref={iframeRef} className="border" />
-                  </div>
-                )}
+                <div className="flex gap-8">
+                  <Button onClick={() => setEtchCreated("")}>Create a new Etch</Button>
+                  <AlertDialogCancel onClick={() => setIsOpen(false)}>Back to Dashboard</AlertDialogCancel>
+                </div>
               </div>
-              <AlertDialogFooter>
-                <AlertDialogCancel disabled={isLoading} onClick={() => setIsOpen(false)}>
-                  Cancel
-                </AlertDialogCancel>
-                <Button isLoading={isLoading} type="submit">
-                  {state === "" ? "Create Etch" : state}
-                </Button>
-              </AlertDialogFooter>
-            </form>
-          </Form>
+            </>
+          ) : (
+            <>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Create New Etch</AlertDialogTitle>
+              </AlertDialogHeader>
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  <div className="mb-2 flex flex-col gap-2 md:flex-row">
+                    <div className="flex w-full flex-col gap-5">
+                      <FormField
+                        control={form.control}
+                        name="etchTitle"
+                        render={({ field }: { field: FieldValues }) => (
+                          <FormItem>
+                            <FormLabel>Public Etch Title</FormLabel>
+                            <FormControl>
+                              <Input disabled={isLoading} id="etchTitle" placeholder="Enter etch title" {...field} />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="etchDescription"
+                        render={({ field }: { field: FieldValues }) => (
+                          <FormItem>
+                            <FormLabel>Public Etch Description (currently not in use)</FormLabel>
+                            <FormControl>
+                              <Input disabled={isLoading} id="etchDescription" placeholder="Enter etch description" {...field} />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="etchFile"
+                        render={({ field }: { field: FieldValues }) => (
+                          <FormItem>
+                            <FormLabel>Upload Etch</FormLabel>
+                            <FormControl>
+                              <Input
+                                type="file"
+                                id="etchFile"
+                                multiple={false}
+                                disabled={isLoading}
+                                onChange={(event) => {
+                                  if (event.target.files?.[0]) {
+                                    field.onChange(event.target.files?.[0]);
+
+                                    if (previewFileTypes.includes(event.target.files?.[0].type.split("/").pop()!)) {
+                                      setFileBlobUrl(URL.createObjectURL(event.target.files?.[0]));
+                                    } else {
+                                      setFileBlobUrl("UNSUPPORTED");
+                                    }
+                                  }
+                                }}
+                              />
+                            </FormControl>
+                            <FormDescription>Etch File: Can be set to private through encryption</FormDescription>
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="etchVisibility"
+                        render={({ field }: { field: FieldValues }) => (
+                          <FormItem className="flex flex-row items-center justify-between rounded-lg">
+                            <div className="space-y-0.5">
+                              <FormLabel className="text-base">Etch Visibility</FormLabel>
+                              <FormDescription>Choose whether your etch is public or private.</FormDescription>
+                            </div>
+                            <FormControl>
+                              <Switch disabled={isLoading} checked={field.value} onCheckedChange={field.onChange} />
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    {fileBlobUrl && fileBlobUrl !== "UNSUPPORTED" && (
+                      <div>
+                        <Label>Preview</Label>
+                        <iframe src={fileBlobUrl} title="Preview" ref={iframeRef} className="border" />
+                      </div>
+                    )}
+                  </div>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel disabled={isLoading} onClick={() => setIsOpen(false)}>
+                      Cancel
+                    </AlertDialogCancel>
+                    <Button isLoading={isLoading} type="submit">
+                      {state === "" ? "Create Etch" : state}
+                    </Button>
+                  </AlertDialogFooter>
+                </form>
+              </Form>
+            </>
+          )}
         </div>
       </AlertDialogContent>
     </AlertDialog>
