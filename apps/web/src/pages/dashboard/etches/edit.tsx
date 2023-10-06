9df709763cd71f468715584dde5985c6474f75a5
Brieyla <business@lancedb.dev>
2023-10-06 10:26:47 +0400

    fixed imports

---
+++ apps/web/src/pages/dashboard/etches/edit.tsx
@@ -0,0 +1,22 @@
+import { DataTableDemo } from "@/components/etches-dashboard-table";
+import { PageBoilerplate } from "@/components/page-boilerplate";
+import EtchSection from "@/components/pages/editEtch/etch-section";
+import { useGetEtchesFromUser } from "@/utils/hooks/useGetEtchesFromUser";
+import { useSession } from "next-auth/react";
+
+export default function EditEtch() {
+  const { data: session } = useSession();
+  const { $state, isLoading, etchToDisplay } = useGetEtchesFromUser(session?.address?.toLowerCase());
+
+  return (
+    <PageBoilerplate>
+      <div className="my-4 items-center px-6 py-6 shadow-etched-1">
+        <div>
+          Client 2 Etch Folder {">"} Blueprints {">"} Chip designs {">"} S2 Chip {">"} Etch file name
+        </div>
+
+        <EtchSection />
+      </div>
+    </PageBoilerplate>
+  );
+}
