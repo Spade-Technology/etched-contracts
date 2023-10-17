118f6611cfbc9c94b2a1a67463bbde501bce5a13
obirijatochukwu <tobirijah@gmail.com>
2023-10-17 04:01:17 -0700

    creation of modify Organization modal

--- apps/web/src/components/dashboard-header.tsx
+++ apps/web/src/components/dashboard-header.tsx
@@ -2,6 +2,7 @@ import { CommandMenu } from "./command";
 import { CreateEtchButton } from "./create-etch-button";
 import { CreateOrgDialog } from "./create-org-dialog";
 import { CreateTeamDialog } from "./create-team-dialog";
+import { EditOrgDialog } from "./edit-org-dialog";
 import { EditTeamDialog } from "./edit-team-dialog";
 import { EtchedENS } from "./ens-button";
 import { TeamSelector } from "./team-selector";
@@ -28,6 +29,29 @@ export const DashboardHeader = () => {
       },
     ],
   };
+
+  const modifyOrgData = {
+    orgName: "SpaceX",
+    teamName: "spade tech",
+    orgMembers: [
+      {
+        id: "0",
+        name: "ex: tom12.etched",
+        role: "admin",
+      },
+      {
+        id: "2",
+        name: "Sophia5678.etched",
+        role: "member",
+      },
+      {
+        id: "3",
+        name: "Olivia3456.etched",
+        role: "admin",
+      },
+    ],
+  };
+
   return (
     <div className="flex h-16 items-center px-6 shadow-etched-1">
       <CreateEtchButton />
@@ -37,6 +61,7 @@ export const DashboardHeader = () => {
       {/* Etched ENS */}
       <EtchedENS />
       <EditTeamDialog modifyTeamData={modifyTeamData} />
+      <EditOrgDialog modifyOrgData={modifyOrgData} />
 
       <div className="mx-6 h-full border-[1px]" />
 
