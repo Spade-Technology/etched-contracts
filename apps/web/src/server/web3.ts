93cca786aac7ec826de76ec0d257055dbea7b997
Brieyla <business@lancedb.dev>
2023-11-08 14:32:36 +0400

    work in progress

--- apps/web/src/server/web3.ts
+++ apps/web/src/server/web3.ts
@@ -1,5 +1,6 @@
 import { env } from "@/env.mjs";
 import { currentChain } from "@/utils/wagmi";
+import { SiweMessage } from "siwe";
 import { Address, createPublicClient, createWalletClient, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 
@@ -13,3 +14,34 @@ export const walletClient = createWalletClient({
   transport: http(env.NEXT_PUBLIC_INFURA_RPC + env.INFURA_KEY),
   account: privateKeyToAccount(env.ETCHED_NODE_PRIVATE_KEY as Address),
 });
+
+export const generateServerAuthSig = async () => {
+  const expiration_time = 60 * 60 * 24 * 7; // 7 days
+  const expiration_date = new Date(Date.now() + expiration_time * 1000);
+  const expiration = expiration_date.toISOString();
+
+  const preparedMessage = {
+    domain: globalThis.location.host,
+    address: (await walletClient.getAddresses())[0],
+    version: "1",
+    chainId: 1,
+    expirationTime: expiration,
+    uri: globalThis.location.href,
+  };
+
+  const message = new SiweMessage(preparedMessage);
+  const body = message.prepareMessage();
+
+  const signedResult = await walletClient.signMessage({ message: body });
+
+  if (!signedResult) throw new Error("Unable to sign message");
+
+  let authSig = {
+    sig: signedResult,
+    derivedVia: "web3.eth.personal.sign",
+    signedMessage: body,
+    address: (await walletClient.getAddresses())[0],
+  };
+
+  return authSig;
+};
