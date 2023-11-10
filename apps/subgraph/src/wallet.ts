bcd42be0dd3a9504c463371a2ab7a499ecbdb24b
Brieyla <business@lancedb.dev>
2023-11-10 15:15:29 +0400

    added eoa to Wallet object

--- apps/subgraph/src/wallet.ts
+++ apps/subgraph/src/wallet.ts
@@ -1,11 +1,14 @@
-import { Address } from "@graphprotocol/graph-ts";
-import { Wallet } from "../generated/schema";
+import { Address } from '@graphprotocol/graph-ts';
+import { Wallet } from '../generated/schema';
 
 export const getOrCreateWallet = (address: Address): Wallet => {
   let wallet = Wallet.load(address);
 
   if (wallet == null) {
     wallet = new Wallet(address);
+
+    wallet.eoa = address.toString();
+
     wallet.save();
   }
 
