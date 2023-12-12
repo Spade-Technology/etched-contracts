9fecb7f84cf913ae09cdf9b4a2e49c509e91695a
Michael Williams <mwilliams.dev@gmail.com>
2023-12-12 00:29:29 -0500

    Initialize subgraph

--- apps/fsdelete/tests/fs-entity-manager-utils.ts
+++ apps/fsdelete/tests/fs-entity-manager-utils.ts
@@ -3,10 +3,15 @@ import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
 import {
   Approval,
   ApprovalForAll,
-  InviteAccepted,
-  InviteDeclined,
-  InviteRevoked,
-  InviteSent,
+  CommentOnEntity,
+  EntityBasePermissionsChanged,
+  EntityCreated,
+  EntityIndividualUserPermissionsChanged,
+  EntityMetaChanged,
+  EntityMoved,
+  EntityShareMaxPermissionsChanged,
+  EntityTransferredToOrganization,
+  OrganizationTransferredToIndividual,
   OwnershipTransferred,
   Transfer
 } from "../generated/FSEntityManager/FSEntityManager"
@@ -58,115 +63,300 @@ export function createApprovalForAllEvent(
   return approvalForAllEvent
 }
 
-export function createInviteAcceptedEvent(
-  inviteId: BigInt,
-  acceptedBy: Address
-): InviteAccepted {
-  let inviteAcceptedEvent = changetype<InviteAccepted>(newMockEvent())
+export function createCommentOnEntityEvent(
+  _entityId: BigInt,
+  _commentIpfsCid: string,
+  _timestamp: BigInt,
+  _commentCount: BigInt
+): CommentOnEntity {
+  let commentOnEntityEvent = changetype<CommentOnEntity>(newMockEvent())
 
-  inviteAcceptedEvent.parameters = new Array()
+  commentOnEntityEvent.parameters = new Array()
 
-  inviteAcceptedEvent.parameters.push(
+  commentOnEntityEvent.parameters.push(
     new ethereum.EventParam(
-      "inviteId",
-      ethereum.Value.fromUnsignedBigInt(inviteId)
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
     )
   )
-  inviteAcceptedEvent.parameters.push(
+  commentOnEntityEvent.parameters.push(
     new ethereum.EventParam(
-      "acceptedBy",
-      ethereum.Value.fromAddress(acceptedBy)
+      "_commentIpfsCid",
+      ethereum.Value.fromString(_commentIpfsCid)
+    )
+  )
+  commentOnEntityEvent.parameters.push(
+    new ethereum.EventParam(
+      "_timestamp",
+      ethereum.Value.fromUnsignedBigInt(_timestamp)
+    )
+  )
+  commentOnEntityEvent.parameters.push(
+    new ethereum.EventParam(
+      "_commentCount",
+      ethereum.Value.fromUnsignedBigInt(_commentCount)
     )
   )
 
-  return inviteAcceptedEvent
+  return commentOnEntityEvent
 }
 
-export function createInviteDeclinedEvent(
-  inviteId: BigInt,
-  declinedBy: Address
-): InviteDeclined {
-  let inviteDeclinedEvent = changetype<InviteDeclined>(newMockEvent())
+export function createEntityBasePermissionsChangedEvent(
+  _entityId: BigInt,
+  _newPermissions: i32
+): EntityBasePermissionsChanged {
+  let entityBasePermissionsChangedEvent = changetype<
+    EntityBasePermissionsChanged
+  >(newMockEvent())
 
-  inviteDeclinedEvent.parameters = new Array()
+  entityBasePermissionsChangedEvent.parameters = new Array()
 
-  inviteDeclinedEvent.parameters.push(
+  entityBasePermissionsChangedEvent.parameters.push(
     new ethereum.EventParam(
-      "inviteId",
-      ethereum.Value.fromUnsignedBigInt(inviteId)
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
     )
   )
-  inviteDeclinedEvent.parameters.push(
+  entityBasePermissionsChangedEvent.parameters.push(
     new ethereum.EventParam(
-      "declinedBy",
-      ethereum.Value.fromAddress(declinedBy)
+      "_newPermissions",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(_newPermissions))
     )
   )
 
-  return inviteDeclinedEvent
+  return entityBasePermissionsChangedEvent
 }
 
-export function createInviteRevokedEvent(
-  inviteId: BigInt,
-  revokedBy: Address
-): InviteRevoked {
-  let inviteRevokedEvent = changetype<InviteRevoked>(newMockEvent())
+export function createEntityCreatedEvent(
+  _entityId: BigInt,
+  _to: Address,
+  _parentId: BigInt,
+  _name: string,
+  _type: i32,
+  _basePermissions: i32
+): EntityCreated {
+  let entityCreatedEvent = changetype<EntityCreated>(newMockEvent())
 
-  inviteRevokedEvent.parameters = new Array()
+  entityCreatedEvent.parameters = new Array()
 
-  inviteRevokedEvent.parameters.push(
+  entityCreatedEvent.parameters.push(
     new ethereum.EventParam(
-      "inviteId",
-      ethereum.Value.fromUnsignedBigInt(inviteId)
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
     )
   )
-  inviteRevokedEvent.parameters.push(
-    new ethereum.EventParam("revokedBy", ethereum.Value.fromAddress(revokedBy))
+  entityCreatedEvent.parameters.push(
+    new ethereum.EventParam("_to", ethereum.Value.fromAddress(_to))
+  )
+  entityCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_parentId",
+      ethereum.Value.fromUnsignedBigInt(_parentId)
+    )
+  )
+  entityCreatedEvent.parameters.push(
+    new ethereum.EventParam("_name", ethereum.Value.fromString(_name))
+  )
+  entityCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_type",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(_type))
+    )
+  )
+  entityCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_basePermissions",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(_basePermissions))
+    )
   )
 
-  return inviteRevokedEvent
+  return entityCreatedEvent
 }
 
-export function createInviteSentEvent(
-  from: Address,
-  externalOrgOrTeamId: BigInt,
-  sourceEntityId: BigInt,
-  baseSharePerms: i32,
-  status: i32
-): InviteSent {
-  let inviteSentEvent = changetype<InviteSent>(newMockEvent())
+export function createEntityIndividualUserPermissionsChangedEvent(
+  _entityId: BigInt,
+  _user: Address,
+  _permissions: i32
+): EntityIndividualUserPermissionsChanged {
+  let entityIndividualUserPermissionsChangedEvent = changetype<
+    EntityIndividualUserPermissionsChanged
+  >(newMockEvent())
 
-  inviteSentEvent.parameters = new Array()
+  entityIndividualUserPermissionsChangedEvent.parameters = new Array()
 
-  inviteSentEvent.parameters.push(
-    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
-  )
-  inviteSentEvent.parameters.push(
+  entityIndividualUserPermissionsChangedEvent.parameters.push(
     new ethereum.EventParam(
-      "externalOrgOrTeamId",
-      ethereum.Value.fromUnsignedBigInt(externalOrgOrTeamId)
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
     )
   )
-  inviteSentEvent.parameters.push(
-    new ethereum.EventParam(
-      "sourceEntityId",
-      ethereum.Value.fromUnsignedBigInt(sourceEntityId)
-    )
+  entityIndividualUserPermissionsChangedEvent.parameters.push(
+    new ethereum.EventParam("_user", ethereum.Value.fromAddress(_user))
   )
-  inviteSentEvent.parameters.push(
+  entityIndividualUserPermissionsChangedEvent.parameters.push(
     new ethereum.EventParam(
-      "baseSharePerms",
-      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(baseSharePerms))
-    )
-  )
-  inviteSentEvent.parameters.push(
-    new ethereum.EventParam(
-      "status",
-      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(status))
+      "_permissions",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(_permissions))
     )
   )
 
-  return inviteSentEvent
+  return entityIndividualUserPermissionsChangedEvent
+}
+
+export function createEntityMetaChangedEvent(
+  _entityId: BigInt,
+  _creator: Address,
+  _documentName: string,
+  _documentDescription: string,
+  _ipfsCid: string,
+  _commentsCount: BigInt,
+  _timestamp: BigInt
+): EntityMetaChanged {
+  let entityMetaChangedEvent = changetype<EntityMetaChanged>(newMockEvent())
+
+  entityMetaChangedEvent.parameters = new Array()
+
+  entityMetaChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
+    )
+  )
+  entityMetaChangedEvent.parameters.push(
+    new ethereum.EventParam("_creator", ethereum.Value.fromAddress(_creator))
+  )
+  entityMetaChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_documentName",
+      ethereum.Value.fromString(_documentName)
+    )
+  )
+  entityMetaChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_documentDescription",
+      ethereum.Value.fromString(_documentDescription)
+    )
+  )
+  entityMetaChangedEvent.parameters.push(
+    new ethereum.EventParam("_ipfsCid", ethereum.Value.fromString(_ipfsCid))
+  )
+  entityMetaChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_commentsCount",
+      ethereum.Value.fromUnsignedBigInt(_commentsCount)
+    )
+  )
+  entityMetaChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_timestamp",
+      ethereum.Value.fromUnsignedBigInt(_timestamp)
+    )
+  )
+
+  return entityMetaChangedEvent
+}
+
+export function createEntityMovedEvent(
+  _entityId: BigInt,
+  _fromParentId: BigInt,
+  _toParentId: BigInt
+): EntityMoved {
+  let entityMovedEvent = changetype<EntityMoved>(newMockEvent())
+
+  entityMovedEvent.parameters = new Array()
+
+  entityMovedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
+    )
+  )
+  entityMovedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_fromParentId",
+      ethereum.Value.fromUnsignedBigInt(_fromParentId)
+    )
+  )
+  entityMovedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_toParentId",
+      ethereum.Value.fromUnsignedBigInt(_toParentId)
+    )
+  )
+
+  return entityMovedEvent
+}
+
+export function createEntityShareMaxPermissionsChangedEvent(
+  _entityId: BigInt,
+  _newPermissions: i32
+): EntityShareMaxPermissionsChanged {
+  let entityShareMaxPermissionsChangedEvent = changetype<
+    EntityShareMaxPermissionsChanged
+  >(newMockEvent())
+
+  entityShareMaxPermissionsChangedEvent.parameters = new Array()
+
+  entityShareMaxPermissionsChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
+    )
+  )
+  entityShareMaxPermissionsChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "_newPermissions",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(_newPermissions))
+    )
+  )
+
+  return entityShareMaxPermissionsChangedEvent
+}
+
+export function createEntityTransferredToOrganizationEvent(
+  _entityId: BigInt,
+  _orgId: BigInt
+): EntityTransferredToOrganization {
+  let entityTransferredToOrganizationEvent = changetype<
+    EntityTransferredToOrganization
+  >(newMockEvent())
+
+  entityTransferredToOrganizationEvent.parameters = new Array()
+
+  entityTransferredToOrganizationEvent.parameters.push(
+    new ethereum.EventParam(
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
+    )
+  )
+  entityTransferredToOrganizationEvent.parameters.push(
+    new ethereum.EventParam("_orgId", ethereum.Value.fromUnsignedBigInt(_orgId))
+  )
+
+  return entityTransferredToOrganizationEvent
+}
+
+export function createOrganizationTransferredToIndividualEvent(
+  _entityId: BigInt,
+  _userId: Address
+): OrganizationTransferredToIndividual {
+  let organizationTransferredToIndividualEvent = changetype<
+    OrganizationTransferredToIndividual
+  >(newMockEvent())
+
+  organizationTransferredToIndividualEvent.parameters = new Array()
+
+  organizationTransferredToIndividualEvent.parameters.push(
+    new ethereum.EventParam(
+      "_entityId",
+      ethereum.Value.fromUnsignedBigInt(_entityId)
+    )
+  )
+  organizationTransferredToIndividualEvent.parameters.push(
+    new ethereum.EventParam("_userId", ethereum.Value.fromAddress(_userId))
+  )
+
+  return organizationTransferredToIndividualEvent
 }
 
 export function createOwnershipTransferredEvent(
