17747b6e644e3c0125a25e826179d7ec25fe59eb
kouraf <farouk.yeddou@gmail.com>
2023-12-18 11:14:25 +0100

    WIP: DO NOT MERGE!!!!

---
+++ apps/web/src/pages/dashboard/3d.tsx
@@ -0,0 +1,6 @@
+import ModelViewer from "@/components/model-viewer";
+
+const Test = () => {
+  return <ModelViewer file="http://localhost:3000/3d/M08.glb" fileName="M08.glb" />;
+};
+export default Test;
