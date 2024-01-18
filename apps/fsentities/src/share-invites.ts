ee2244035e3c40ac47c8097872177c499d900a87
Michael Williams <mwilliams.dev@gmail.com>
2024-01-17 19:43:05 -0500

    FSEntities ABI and handler updates

--- apps/fsentities/src/share-invites.ts
+++ apps/fsentities/src/share-invites.ts
@@ -74,34 +74,3 @@ export function handleInviteSent(event: InviteSentEvent): void {
 
   entity.save()
 }
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
