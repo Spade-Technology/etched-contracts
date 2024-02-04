6ddcb19c10c88bdd44e42fe83538685a1b26f8f9
Brieyla <business@lancedb.dev>
2024-02-04 16:09:36 +0400

    Fixed build

--- apps/web/src/utils/theme.tsx
+++ apps/web/src/utils/theme.tsx
@@ -10,13 +10,20 @@ export const ThemeContext = React.createContext({
 // Define a ThemeProvider component that wraps its children in ThemeContext.Provider
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   // Retrieve the current theme from local storage or default to 'light'
-  const [theme, setTheme] = useState<string>(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState<string>("light");
 
   // Effect to apply the theme class to the root element
   useEffect(() => {
     localStorage.setItem("theme", theme);
     document.documentElement.className = theme;
   }, [theme]);
+
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    if (localTheme) {
+      setTheme(localTheme);
+    }
+  }, []);
 
   // Return the context provider with the theme and updateTheme function
   return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
