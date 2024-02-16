c139174addbae91d8db3154433475e90fc79b14d
Brieyla <business@lancedb.dev>
2024-02-16 17:39:17 +0400

    added get all tags of owner funciton and using it at tag assignement

---
+++ apps/web/src/utils/hooks/useGetTagsOfEtchAndOwner.ts
@@ -0,0 +1,50 @@
+// File 1: web/src/utils/hooks/useGetTagsOfEtchAndOwner.ts
+import { useContext, useEffect } from "react";
+import { gql, useQuery } from "urql";
+import { refetchContext } from "../urql";
+
+import { Client } from "urql";
+import { urqlConfig } from "../urql";
+
+const TAG_OF_ETCH_AND_OWNER = gql`
+  query GetTagsOfEtchAndOwner($owner: String!, $id: ID!) {
+    etch(id: $id) {
+      id
+      tags(where: { owner: $owner }) {
+        tag
+        id
+        owner {
+          eoa
+        }
+      }
+    }
+  }
+`;
+
+export const getTagsOfEtchAndOwner = (etchId: string, address: string) => {
+  const client = new Client(urqlConfig);
+
+  return client.query(TAG_OF_ETCH_AND_OWNER, {
+    id: etchId + "-Etch",
+    owner: address,
+  });
+};
+
+export const useGetTagsOfEtchAndOwner = (etchId?: string, address?: string) => {
+  const [{ data, fetching, error, operation }, reexecute] = useQuery({
+    query: TAG_OF_ETCH_AND_OWNER,
+    variables: { id: etchId!, owner: address! },
+    pause: !etchId || !address,
+  });
+
+  const refetch = () => reexecute({ requestPolicy: "network-only" });
+
+  const { setRefetchTagsOfEtchAndOwner } = useContext(refetchContext);
+  useEffect(() => {
+    if (setRefetchTagsOfEtchAndOwner) {
+      setRefetchTagsOfEtchAndOwner(refetch);
+    }
+  }, [operation, setRefetchTagsOfEtchAndOwner]);
+
+  return { data, fetching, error, refetch };
+};
---
+++ apps/web/src/utils/hooks/useGetTagsOfOwner.ts
@@ -0,0 +1,44 @@
+import { useContext, useEffect } from "react";
+import { gql, useQuery } from "urql";
+import { refetchContext } from "../urql";
+
+import { Client } from "urql";
+import { urqlConfig } from "../urql";
+
+const TAGS_OF_OWNER = gql`
+  query GetTagsOfOwner($owner: String!) {
+    tags(where: { owner: $owner }) {
+      tag
+      id
+      owner {
+        eoa
+      }
+    }
+  }
+`;
+
+export const getTagsOfOwner = (address: string) => {
+  const client = new Client(urqlConfig);
+
+  return client.query(TAGS_OF_OWNER, {
+    owner: address,
+  });
+};
+
+export const useGetTagsOfOwner = (address?: string) => {
+  const [{ data, fetching, error, operation }, reexecute] = useQuery({
+    query: TAGS_OF_OWNER,
+    variables: { owner: address },
+  });
+
+  const refetch = () => reexecute({ requestPolicy: "network-only" });
+
+  const { setRefetchTagsOfOwner } = useContext(refetchContext);
+  useEffect(() => {
+    if (setRefetchTagsOfOwner) {
+      setRefetchTagsOfOwner(refetch);
+    }
+  }, [operation, setRefetchTagsOfOwner]);
+
+  return { data: data?.tags.filter((tag: any) => tag?.tag?.length > 0), fetching, error, refetch };
+};
