e67387501bdec420676daa9122919c438da6f850
Michael Williams <mwilliams.dev@gmail.com>
2024-05-03 20:42:18 -0400

    It appears that [trpc].ts is the place to add maxDuration to the config object.  Testing a final time.

--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -3,8 +3,8 @@ import { appRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-export const config = { maxDuration: 299, dynamic: 'force-dynamic' };
-export const maxDuration = 299; // This function can run for a maximum of 5 seconds
+export const config = { maxDuration: 100, dynamic: 'force-dynamic' };
+export const maxDuration = 100; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
 
 // export API handler
--- apps/web/src/server/api/routers/etch.ts
+++ apps/web/src/server/api/routers/etch.ts
@@ -15,11 +15,6 @@ import { Address, decodeEventLog, encodeFunctionData, encodePacked, keccak256 } 
 import { z } from "zod";
 const random = require("random-bigint");
 
-export const config = { maxDuration: 299, dynamic: 'force-dynamic' };
-export const maxDuration = 299; // This function can run for a maximum of 5 seconds
-export const dynamic = 'force-dynamic';
-
-
 export const etchRouter = createTRPCRouter({
   bulkMintEtch: protectedProcedure
     .input(
