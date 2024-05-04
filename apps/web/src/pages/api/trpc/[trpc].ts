5a3b8681fdabb5261cb1ff32685704eac7333268
Michael Williams <mwilliams.dev@gmail.com>
2024-05-04 01:02:52 -0400

    Small adjustment to timeout.

--- apps/web/src/pages/api/trpc/[trpc].ts
+++ apps/web/src/pages/api/trpc/[trpc].ts
@@ -4,8 +4,6 @@ import { createTRPCContext } from "@/server/api/trpc";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 export const config = { maxDuration: 100, dynamic: 'force-dynamic' };
-export const maxDuration = 100; // This function can run for a maximum of 5 seconds
-export const dynamic = 'force-dynamic';
 
 // export API handler
 export default createNextApiHandler({
