84c082344680b9d35ca22bd6553ec0e0633be242
Michael Williams <mwilliams.dev@gmail.com>
2024-05-08 09:23:09 -0400

    More debug.

--- apps/web/src/lit.ts
+++ apps/web/src/lit.ts
@@ -53,6 +53,7 @@ class Lit {
     const client = await this.connect();
     console.log(`Inside "decryptFromIpfs" 2`)
     const ipfsData = await (await fetch(`${ipfsPlublicClientUrl}${props.ipfsCid}`)).json();
+    console.log(`IPFS: ${ipfsPlublicClientUrl}${props.ipfsCid}`)
     console.log(`Inside "decryptFromIpfs" 3`)
     const data: Parameters<typeof LitJsSdk.decryptToFile>[0] = {
       authSig: props.authSig,
